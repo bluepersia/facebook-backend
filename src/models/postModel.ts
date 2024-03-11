5f6fd2ed33aaa6fde255e5c8df4fda5e4cddabad
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-11 12:29:49 -0600

    Post model

---
+++ src/models/postModel.ts
@@ -0,0 +1,52 @@
+import { Schema, Types, model } from "mongoose";
+
+
+export interface IPost
+{
+    user: Types.ObjectId,
+    text: string,
+    images: Types.ObjectId[],
+    createdAt:Date
+}
+
+
+const postSchema = new Schema<IPost> ({
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Post must belong to a user']
+    },
+    text: {
+        type:String,
+        validate: {
+            validator: function (val:string) : boolean
+            {
+                return Boolean (val || this.images.length > 0);
+            },
+
+            message: 'Post must contain either text or images'
+        }
+    },
+    images: {   
+        type: [{
+            type: Schema.ObjectId,
+            ref: 'Image'
+        }],
+        validate: {
+            validator: function (val:Types.ObjectId[]) :boolean
+            {
+                return val.length > 0 || this.text;
+            },
+            message: 'Post must contain either text or images'
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now ()
+    }
+})
+
+
+const Post = model ('Post', postSchema);
+
+export default Post;
